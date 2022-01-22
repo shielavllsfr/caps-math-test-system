903e2a57be1ee55565891e8508eaa303482b385e
Shiela Villasfir <shielavillasfir01@gmail.com>
2022-01-22 15:42:33 +0800

    update firebase

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
     firebase.auth().onAuthStateChanged(function (signInUser) {
       if (signInUser) {
         let admin = false;
-        if (signInUser.uid === "8XjaEXBOuATikvqtg2nJEy1UWWt1") admin = true;
+        if (signInUser.uid === "Kky5YznWr2UiE6FncgFtxbA9eXq2") admin = true;
         setValues({
           isAuth: true,
           user: signInUser,
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/compat';
+import firebase from "firebase/compat";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,17 +7,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTWFFHQ11qqjFSsGUSf2RTe68LgVs4ZsU",
-  authDomain: "capstone-math-test-system.firebaseapp.com",
-  projectId: "capstone-math-test-system",
-  storageBucket: "capstone-math-test-system.appspot.com",
-  messagingSenderId: "1059985814882",
-  appId: "1:1059985814882:web:44c0b8119b3a5a3709283b",
-  measurementId: "G-X220S6Z3F8"
+  apiKey: "AIzaSyDvlNZ-wLU3zRJ334v19Io0n86xaDY9jGM",
+  authDomain: "capstone-system-aaa2b.firebaseapp.com",
+  projectId: "capstone-system-aaa2b",
+  storageBucket: "capstone-system-aaa2b.appspot.com",
+  messagingSenderId: "398693770773",
+  appId: "1:398693770773:web:8ec81c085fdae4d70b101c",
+  measurementId: "G-2Q3MZZZ1QD",
 };
 
 // Initialize Firebase
 getAnalytics(firebase.initializeApp(firebaseConfig));
-
 
 export default firebase;
